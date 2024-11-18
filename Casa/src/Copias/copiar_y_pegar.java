58d3d0b1bb52fec71a99fb4e539d49a4ec1fe4db
swilc <swilc@HP-Victus>
2024-11-18 10:23:45 +0100

    ya

--- Casa/src/Copias/copiar_y_pegar.java
+++ Casa/src/Copias/copiar_y_pegar.java
@@ -6,6 +6,50 @@ public class copiar_y_pegar {
 		// TODO Auto-generated method stub
 System.out.println("busca esto");
 /*el programa que ordena en ascendente un arrary de números enteros utilizando el algoritmo de la burbuja.*/
+	
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        // Ejemplo de array de números enteros
+        int[] numbers = {5, 3, 8, 6, 2, 7, 4, 1};
+
+        System.out.println("Array original: " + Arrays.toString(numbers));
+        
+        // Llamamos a la función de ordenamiento
+        bubbleSort(numbers);
+
+        System.out.println("Array ordenado: " + Arrays.toString(numbers));
+    }
+
+    // Método que implementa el algoritmo de la burbuja
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+
+        // Iterar por el arreglo
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            // Comparar y intercambiar elementos adyacentes
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Intercambiar los elementos
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // Si no hubo intercambios en una pasada, el arreglo ya está ordenado
+            if (!swapped) break;
+        }
+    }
+}
+
+	
 	}
 
 }
+
