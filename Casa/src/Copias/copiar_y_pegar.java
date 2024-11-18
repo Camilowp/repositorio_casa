908b0779014d1d6c6574062f58642da8443a34b9
Camilo <wcamilo446@gmail.com>
2024-11-18 10:18:38 +0100

    hazlo

--- Casa/src/Copias/copiar_y_pegar.java
+++ Casa/src/Copias/copiar_y_pegar.java
@@ -4,7 +4,8 @@ public class copiar_y_pegar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hola mundo");
+System.out.println("busca esto");
+/*el programa que ordena en ascendente un arrary de números enteros utilizando el algoritmo de la burbuja.*/
 	}
 
 }
