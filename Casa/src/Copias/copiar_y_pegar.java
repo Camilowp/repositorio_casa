23b1bad822ab2d79b7efb72235e59c281ee08995
swilc <swilc@HP-Victus>
2025-01-28 19:22:44 +0100

    ale

--- Casa/src/Copias/copiar_y_pegar.java
+++ Casa/src/Copias/copiar_y_pegar.java
@@ -8,8 +8,4 @@ package Copias;
 // hjgjhgjgg YAAAAAAAAAA
 
 
-//GRACIAS SOFA YA ME APARECE TODO, SIGAMOS HABLANDO POR ACÁ 
-// QUE PASO co la mama que no te habia respondido 
-// 	QUE PASO DE QUE? DAME CONTEXTO 
-// que ud dijo que estaba brava 
-//porque anoche nos escuchó hablando a todos y despues a mi con sofia 
+// bueno luego me cuneta con calma y lujo de detalle
