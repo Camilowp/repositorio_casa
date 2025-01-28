6dd7de9446740a850ddbe7e60e8131240d940ef1
swilc <swilc@HP-Victus>
2025-01-28 19:20:29 +0100

    ??

--- Casa/src/Copias/copiar_y_pegar.java
+++ Casa/src/Copias/copiar_y_pegar.java
@@ -11,3 +11,4 @@ package Copias;
 //GRACIAS SOFA YA ME APARECE TODO, SIGAMOS HABLANDO POR ACÁ 
 // QUE PASO co la mama que no te habia respondido 
 // 	QUE PASO DE QUE? DAME CONTEXTO 
+// que ud dijo que estaba brava 
