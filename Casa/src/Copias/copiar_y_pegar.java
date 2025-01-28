859d95c3bdc6d53a54075f8243b2423cf66da1d0
swilc <swilc@HP-Victus>
2025-01-28 19:39:28 +0100

    k

--- Casa/src/Copias/copiar_y_pegar.java
+++ Casa/src/Copias/copiar_y_pegar.java
@@ -3,11 +3,4 @@ package Copias;
 //BORRE TODO LO DE ANTTES 
 // ahora si 
 /// hjhj
-// sofa esta es una prueba
-
-// hjgjhgjgg YAAAAAAAAAA
-
-
-// bueno luego me cuneta con calma y lujo de detalle
-
-//DALE, PREPARATE QUE A Y MEDIA EMPEZAMOS 
+//
